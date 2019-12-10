a50e10fe2dadc2e108888595d7b8fce15b6a63e6
AvivSham <Mista2311@gmail.com>
2019-12-10 11:31:49 +0200

    update

--- nice.py
+++ nice.py
@@ -1,19 +1,14 @@
-"""NICE model
+"""Utility classes for NICE.
 """
 
 import torch
 import torch.nn as nn
-from torch.distributions.transforms import Transform,SigmoidTransform,AffineTransform
-from torch.distributions import Uniform, TransformedDistribution
-import numpy as np
 
 """Additive coupling layer.
 """
-
-
 class AdditiveCoupling(nn.Module):
     def __init__(self, in_out_dim, mid_dim, hidden, mask_config):
-        """Initialize an additive coupling layer.
+        """Initialize a coupling layer.
 
         Args:
             in_out_dim: input/output dimensions.
@@ -23,33 +18,36 @@ class AdditiveCoupling(nn.Module):
         """
         super(AdditiveCoupling, self).__init__()
         self.mask_config = mask_config
-        self.in_block = nn.Sequential(nn.ReLU(nn.Linear(in_out_dim//2, mid_dim)))
-        self.mid_block = nn.ModuleList([nn.Sequential(nn.ReLU(nn.Linear(mid_dim, mid_dim)))\
-                                        for _ in range(hidden-1)])
+
+        self.in_block = nn.Sequential(
+            nn.Linear(in_out_dim//2, mid_dim),
+            nn.ReLU())
+        self.mid_block = nn.ModuleList([
+            nn.Sequential(
+                nn.Linear(mid_dim, mid_dim),
+                nn.ReLU()) for _ in range(hidden - 1)])
         self.out_block = nn.Linear(mid_dim, in_out_dim//2)
 
-    def forward(self, x, log_det_J, reverse=False):
+    def forward(self, x, reverse=False):
         """Forward pass.
 
         Args:
             x: input tensor.
-            log_det_J: log determinant of the Jacobian
             reverse: True in inference mode, False in sampling mode.
         Returns:
-            transformed tensor and log-determinant of Jacobian.
+            transformed tensor.
         """
         [B, W] = list(x.size())
-        x = x.reshape((B,W//2,2))
+        x = x.reshape((B, W//2, 2))
         if self.mask_config:
             on, off = x[:, :, 0], x[:, :, 1]
         else:
-            on, off = x[:, :, 1], x[:, :, 0]
+            off, on = x[:, :, 0], x[:, :, 1]
 
         off_ = self.in_block(off)
         for i in range(len(self.mid_block)):
             off_ = self.mid_block[i](off_)
         shift = self.out_block(off_)
-
         if reverse:
             on = on - shift
         else:
@@ -59,66 +57,10 @@ class AdditiveCoupling(nn.Module):
             x = torch.stack((on, off), dim=2)
         else:
             x = torch.stack((off, on), dim=2)
-
-        return x.reshape((B, W)), log_det_J
-
-
-class AffineCoupling(nn.Module):
-    def __init__(self, in_out_dim, mid_dim, hidden, mask_config):
-        """Initialize an affine coupling layer.
-
-        Args:
-            in_out_dim: input/output dimensions.
-            mid_dim: number of units in a hidden layer.
-            hidden: number of hidden layers.
-            mask_config: 1 if transform odd units, 0 if transform even units.
-        """
-        super(AffineCoupling, self).__init__()
-        self.mask_config = mask_config
-        self.in_block = nn.Sequential(nn.ReLU(nn.Linear(in_out_dim//2, mid_dim)))
-        self.mid_block = nn.ModuleList([nn.Sequential(nn.ReLU(nn.Linear(mid_dim, mid_dim))) \
-                                        for _ in range(hidden-1)])
-        self.out_block = nn.Linear(mid_dim, in_out_dim//2)
-
-    def forward(self, x, log_det_J, reverse=False):
-        """Forward pass.
-
-        Args:
-            x: input tensor.
-            log_det_J: log determinant of the Jacobian
-            reverse: True in inference mode, False in sampling mode.
-        Returns:
-            transformed tensor and log-determinant of Jacobian.
-        """
-
-        [B, W] = list(x.size())
-        x = x.view((B,W//2,2))
-        if self.mask_config:
-            on, off = x[:, :, 0], x[:, :, 1]
-        else:
-            on, off = x[:, :, 1], x[:, :, 0]
-
-        off_ = self.in_block(off)
-        for i in range(len(self.mid_block)):
-            off_ = self.mid_block[i](off_)
-        shift = self.out_block(off_)
-        shift_1, shift_2 = shift.chunk(2,1)
-        if reverse:
-            on = on / shift_1 - shift_2
-        else:
-            on = shift_1 * on + shift_2
-
-        if self.mask_config:
-            x = torch.stack((on, off), dim=2)
-        else:
-            x = torch.stack((off, on), dim=2)
-        log_det_J = torch.sum(torch.log(shift_1).view(input.shape[0], -1), 1)
-        return x.view((B, W)), log_det_J
+        return x.reshape((B, W))
 
 """Log-scaling layer.
 """
-
-
 class Scaling(nn.Module):
     def __init__(self, dim):
         """Initialize a (log-)scaling layer.
@@ -129,7 +71,6 @@ class Scaling(nn.Module):
         super(Scaling, self).__init__()
         self.scale = nn.Parameter(
             torch.zeros((1, dim)), requires_grad=True)
-        self.eps = 1e-5
 
     def forward(self, x, reverse=False):
         """Forward pass.
@@ -142,22 +83,20 @@ class Scaling(nn.Module):
         """
         log_det_J = torch.sum(self.scale)
         if reverse:
-            scale = torch.exp(-self.scale)+ self.eps
+            x = x * torch.exp(-self.scale)
         else:
-            scale = torch.exp(self.scale) + self.eps
-        x *= scale
+            x = x * torch.exp(self.scale)
         return x, log_det_J
-
 
 """NICE main model.
 """
-
-
 class NICE(nn.Module):
-    def __init__(self, prior, coupling, in_out_dim, mid_dim, hidden, mask_config,device, coup_type = "additive"):
+    def __init__(self, prior, coupling,
+                 in_out_dim, mid_dim, hidden, mask_config):
         """Initialize a NICE.
 
         Args:
+            prior: prior distribution over latent space Z.
             coupling: number of coupling layers.
             in_out_dim: input/output dimensions.
             mid_dim: number of units in a hidden layer.
@@ -165,28 +104,18 @@ class NICE(nn.Module):
             mask_config: 1 if transform odd units, 0 if transform even units.
         """
         super(NICE, self).__init__()
-        self.device = device
-        if prior == 'gaussian':
-            self.prior = torch.distributions.Normal(
-                torch.tensor(0.).to(device), torch.tensor(1.).to(device))
-        elif prior == 'logistic':
-            self.prior = TransformedDistribution(Uniform(0, 1),
-                                                 [SigmoidTransform().inv,
-                                                  AffineTransform(loc=0., scale=1.)])
-        else:
-            raise ValueError('Prior not implemented.')
+        self.prior = prior
         self.in_out_dim = in_out_dim
-        if coup_type == "additive":
-            self.coupling = nn.ModuleList([AdditiveCoupling(in_out_dim=in_out_dim, mid_dim=mid_dim,
-                                                            hidden=hidden, mask_config=(mask_config+i)%2)\
-                                           for i in range(coupling)])
-        else:
-            self.coupling = nn.ModuleList([AffineCoupling(in_out_dim=in_out_dim, mid_dim=mid_dim,
-                                                          hidden=hidden, mask_config=(mask_config+i)%2)\
-                                           for i in range(coupling)])
+
+        self.coupling = nn.ModuleList([
+            AdditiveCoupling(in_out_dim=in_out_dim,
+                     mid_dim=mid_dim,
+                     hidden=hidden,
+                     mask_config=(mask_config+i)%2) \
+            for i in range(coupling)])
         self.scaling = Scaling(in_out_dim)
 
-    def f_inverse(self, z):
+    def g(self, z):
         """Transformation g: Z -> X (inverse of f).
 
         Args:
@@ -194,10 +123,10 @@ class NICE(nn.Module):
         Returns:
             transformed tensor in data space X.
         """
-        x, log_det_J = self.scaling(z, reverse=True)
+        x, _ = self.scaling(z, reverse=True)
         for i in reversed(range(len(self.coupling))):
-            x, log_det_J = self.coupling[i](x, log_det_J, reverse = True)
-        return x, log_det_J
+            x = self.coupling[i](x, reverse=True)
+        return x
 
     def f(self, x):
         """Transformation f: X -> Z (inverse of g).
@@ -205,11 +134,10 @@ class NICE(nn.Module):
         Args:
             x: tensor in data space X.
         Returns:
-            transformed tensor in latent space Z and log determinant Jacobian
+            transformed tensor in latent space Z.
         """
-        log_det_J = 0
         for i in range(len(self.coupling)):
-            x, log_det_J = self.coupling[i](x, log_det_J)
+            x = self.coupling[i](x)
         return self.scaling(x)
 
     def log_prob(self, x):
@@ -223,7 +151,6 @@ class NICE(nn.Module):
             log-likelihood of input.
         """
         z, log_det_J = self.f(x)
-        log_det_J -= np.log(256)*self.in_out_dim #log det for rescaling from [0.256] (after dequantization) to [0,1]
         log_ll = torch.sum(self.prior.log_prob(z), dim=1)
         return log_ll + log_det_J
 
@@ -235,8 +162,8 @@ class NICE(nn.Module):
         Returns:
             samples from the data space X.
         """
-        z = self.prior.sample((size, self.in_out_dim)).to(self.device)
-        return self.f_inverse(z)
+        z = self.prior.sample((size, self.in_out_dim)).cuda()
+        return self.g(z)
 
     def forward(self, x):
         """Forward pass.
@@ -247,7 +174,3 @@ class NICE(nn.Module):
             log-likelihood of input.
         """
         return self.log_prob(x)
-
-
-
-
--- train_model.py
+++ train_model.py
@@ -1,17 +1,17 @@
 """Training procedure for NICE.
 """
-
-import argparse
-import torch, torchvision
-from torchvision import transforms
-import numpy as np
-import nice, utils
-import os
-import matplotlib.pyplot as plt
-
-N = 100
-
-
+#
+# import argparse
+# import torch, torchvision
+# from torchvision import transforms
+# import numpy as np
+# import nice, utils
+# import os
+# import matplotlib.pyplot as plt
+#
+# N = 100
+#
+#
 def train(flow, trainloader, optimizer, device):
     flow.train()  # set to training mode
     running_loss = 0
@@ -43,55 +43,183 @@ def test(flow, testloader, epoch, filename, device):
             running_loss += float(loss)
     return running_loss / n_batches
 
+#
+# def main(args):
+#     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+#     full_dim = 28 * 28
+#     transform = transforms.Compose([
+#         transforms.ToTensor(),
+#         transforms.Normalize((0.5,), (1.,)),
+#         transforms.Lambda(lambda x: x + torch.zeros_like(x).uniform_(0., 1./256.)) #dequantization
+#     ])
+#
+#     if args.dataset == 'mnist':
+#         trainset = torchvision.datasets.MNIST(root='./data/MNIST',
+#             train=True, download=True, transform=transform)
+#         trainloader = torch.utils.data.DataLoader(trainset,
+#             batch_size=args.batch_size, shuffle=True, num_workers=1)
+#         testset = torchvision.datasets.MNIST(root='./data/MNIST',
+#             train=False, download=True, transform=transform)
+#         testloader = torch.utils.data.DataLoader(testset,
+#             batch_size=args.batch_size, shuffle=False, num_workers=1)
+#     elif args.dataset == 'fashion-mnist':
+#         trainset = torchvision.datasets.FashionMNIST(root='./data/FashionMNIST',
+#             train=True, download=True, transform=transform)
+#         trainloader = torch.utils.data.DataLoader(trainset,
+#             batch_size = args.batch_size, shuffle=True, num_workers=2)
+#         testset = torchvision.datasets.FashionMNIST(root='./data/FashionMNIST',
+#             train=False, download=True, transform=transform)
+#         testloader = torch.utils.data.DataLoader(testset,
+#             batch_size=args.batch_size, shuffle=False, num_workers=2)
+#     else:
+#         raise ValueError('Dataset not implemented')
+#
+#     model_save_filename = '%s_' % args.dataset \
+#              + 'batch%d_' % args.batch_size \
+#              + 'coupling%d_' % args.coupling \
+#              + 'mid%d_' % args.mid_dim \
+#              + 'hidden%d_' % args.hidden \
+#              + '.pt'
+#
+#     flow = nice.NICE(
+#                 prior=args.prior,
+#                 coupling=args.coupling,
+#                 in_out_dim=full_dim,
+#                 mid_dim=args.mid_dim,
+#                 hidden=args.hidden,
+#                 mask_config=1.,
+#                 device=device,
+#                 coup_type=args.coup_type).to(device)
+#     optimizer = torch.optim.Adam(
+#         flow.parameters(), lr=args.lr)
+#
+
+
+# if __name__ == '__main__':
+#     parser = argparse.ArgumentParser('')
+#     parser.add_argument('--dataset',
+#                         help='dataset to be modeled.',
+#                         type=str,
+#                         default='mnist')
+#     parser.add_argument('--prior',
+#                         help='latent distribution.',
+#                         type=str,
+#                         default='logistic')
+#     parser.add_argument('--batch_size',
+#                         help='number of images in a mini-batch.',
+#                         type=int,
+#                         default=128)
+#     parser.add_argument('--epochs',
+#                         help='maximum number of iterations.',
+#                         type=int,
+#                         default=50)
+#     parser.add_argument('--sample_size',
+#                         help='number of images to generate.',
+#                         type=int,
+#                         default=64)
+#     parser.add_argument('--coupling',
+#                         help='.',
+#                         type=int,
+#                         default=4)
+#     parser.add_argument('--mid-dim',
+#                         help='.',
+#                         type=int,
+#                         default=1000)
+#     parser.add_argument('--hidden',
+#                         help='.',
+#                         type=int,
+#                         default=5)
+#     parser.add_argument('--lr',
+#                         help='initial learning rate.',
+#                         type=float,
+#                         default=1e-3)
+#     parser.add_argument('--save-every',
+#                         help='every how many epochs to save the model',
+#                         type=float,
+#                         default=1e+3)
+#     parser.add_argument('--coup-type',
+#                         help="coupling type",
+#                         type=str,
+#                         default="additive")
+#     args = parser.parse_args()
+#     main(args)
+
+
+
+"""Training procedure for NICE.
+"""
+
+import argparse
+import torch, torchvision
+import numpy as np
+import nice, utils
+import matplotlib.pyplot as plt
 
 def main(args):
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    full_dim = 28 * 28
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.5,), (1.,)),
-        transforms.Lambda(lambda x: x + torch.zeros_like(x).uniform_(0., 1./256.)) #dequantization
-    ])
+    device = torch.device("cuda:0")
 
-    if args.dataset == 'mnist':
-        trainset = torchvision.datasets.MNIST(root='./data/MNIST',
-            train=True, download=True, transform=transform)
+    # model hyperparameters
+    dataset = args.dataset
+    batch_size = args.batch_size
+    latent = args.latent
+    max_iter = args.max_iter
+    sample_size = args.sample_size
+    coupling = 4
+    mask_config = 1.
+
+    # optimization hyperparameters
+    lr = args.lr
+    momentum = args.momentum
+    decay = args.decay
+
+    zca = None
+    mean = None
+    if dataset == 'mnist':
+        mean = torch.load('./statistics/mnist_mean.pt')
+        (full_dim, mid_dim, hidden) = (1 * 28 * 28, 1000, 5)
+        transform = torchvision.transforms.ToTensor()
+        trainset = torchvision.datasets.MNIST(root='~/torch/data/MNIST',
+                                              train=True, download=True, transform=transform)
         trainloader = torch.utils.data.DataLoader(trainset,
-            batch_size=args.batch_size, shuffle=True, num_workers=1)
+                                                  batch_size=batch_size, shuffle=True, num_workers=2)
         testset = torchvision.datasets.MNIST(root='./data/MNIST',
-            train=False, download=True, transform=transform)
+                                             train=False, download=True, transform=transform)
         testloader = torch.utils.data.DataLoader(testset,
-            batch_size=args.batch_size, shuffle=False, num_workers=1)
-    elif args.dataset == 'fashion-mnist':
-        trainset = torchvision.datasets.FashionMNIST(root='./data/FashionMNIST',
-            train=True, download=True, transform=transform)
+                                             batch_size=args.batch_size, shuffle=False, num_workers=1)
+
+    elif dataset == 'fashion-mnist':
+        mean = torch.load('./statistics/fashion_mnist_mean.pt')
+        (full_dim, mid_dim, hidden) = (1 * 28 * 28, 1000, 5)
+        transform = torchvision.transforms.ToTensor()
+        trainset = torchvision.datasets.FashionMNIST(root='~/torch/data/FashionMNIST',
+                                                     train=True, download=True, transform=transform)
         trainloader = torch.utils.data.DataLoader(trainset,
-            batch_size = args.batch_size, shuffle=True, num_workers=2)
-        testset = torchvision.datasets.FashionMNIST(root='./data/FashionMNIST',
-            train=False, download=True, transform=transform)
+                                                  batch_size=batch_size, shuffle=True, num_workers=2)
+        testset = torchvision.datasets.MNIST(root='./data/FashionMNIST',
+                                             train=False, download=True, transform=transform)
         testloader = torch.utils.data.DataLoader(testset,
-            batch_size=args.batch_size, shuffle=False, num_workers=2)
-    else:
-        raise ValueError('Dataset not implemented')
+                                             batch_size=args.batch_size, shuffle=False, num_workers=1)
+    if latent == 'normal':
+        prior = torch.distributions.Normal(
+            torch.tensor(0.).to(device), torch.tensor(1.).to(device))
+    elif latent == 'logistic':
+        prior = utils.StandardLogistic()
 
     model_save_filename = '%s_' % args.dataset \
-             + 'batch%d_' % args.batch_size \
-             + 'coupling%d_' % args.coupling \
-             + 'mid%d_' % args.mid_dim \
-             + 'hidden%d_' % args.hidden \
-             + '.pt'
+                          + 'batch%d_' % args.batch_size \
+                          + 'coupling%d_' % args.coupling \
+                          + 'mid%d_' % args.mid_dim \
+                          + 'hidden%d_' % args.hidden \
+                          + '.pt'
 
-    flow = nice.NICE(
-                prior=args.prior,
-                coupling=args.coupling,
-                in_out_dim=full_dim, 
-                mid_dim=args.mid_dim,
-                hidden=args.hidden,
-                mask_config=1.,
-                device=device,
-                coup_type=args.coup_type).to(device)
+    flow = nice.NICE(prior=prior,
+                     coupling=coupling,
+                     in_out_dim=full_dim,
+                     mid_dim=mid_dim,
+                     hidden=hidden,
+                     mask_config=mask_config).to(device)
     optimizer = torch.optim.Adam(
-        flow.parameters(), lr=args.lr)
+        flow.parameters(), lr=lr, betas=(momentum, decay), eps=1e-4)
 
     train_loss = []
     test_loss   = []
@@ -112,52 +240,39 @@ def main(args):
     ax.legend(["train loss","test loss"])
     plt.savefig(os.path.join(os.getcwd(),"loss.png"))
 
-
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser('')
+    parser = argparse.ArgumentParser('MNIST NICE PyTorch implementation')
     parser.add_argument('--dataset',
                         help='dataset to be modeled.',
                         type=str,
                         default='mnist')
-    parser.add_argument('--prior',
-                        help='latent distribution.',
-                        type=str,
-                        default='logistic')
     parser.add_argument('--batch_size',
                         help='number of images in a mini-batch.',
                         type=int,
-                        default=128)
-    parser.add_argument('--epochs',
+                        default=200)
+    parser.add_argument('--latent',
+                        help='latent distribution.',
+                        type=str,
+                        default='logistic')
+    parser.add_argument('--max_iter',
                         help='maximum number of iterations.',
                         type=int,
-                        default=50)
+                        default=25000)
     parser.add_argument('--sample_size',
                         help='number of images to generate.',
                         type=int,
                         default=64)
-    parser.add_argument('--coupling',
-                        help='.',
-                        type=int,
-                        default=4)
-    parser.add_argument('--mid-dim',
-                        help='.',
-                        type=int,
-                        default=1000)
-    parser.add_argument('--hidden',
-                        help='.',
-                        type=int,
-                        default=5)
     parser.add_argument('--lr',
                         help='initial learning rate.',
                         type=float,
                         default=1e-3)
-    parser.add_argument('--save-every',
-                        help='every how many epochs to save the model',
+    parser.add_argument('--momentum',
+                        help='beta1 in Adam optimizer.',
                         type=float,
-                        default=1e+3)
-    parser.add_argument('--coup-type',
-                        help="coupling type",
-                        type=str,
-                        default="additive")
+                        default=0.9)
+    parser.add_argument('--decay',
+                        help='beta2 in Adam optimizer.',
+                        type=float,
+                        default=0.999)
     args = parser.parse_args()
     main(args)
